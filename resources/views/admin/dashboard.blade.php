06ee2b56d2611c082d906602b90a700bd0544e83
SATRIO <satriotol69@gmail.com>
2022-05-05 22:10:24 +0700

    fixing

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -66,7 +66,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-3">
                 <div class="card">
                     <div class="card-header">
                         <h4>Media Sosial</h4>
@@ -76,7 +76,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-9">
                 <div class="card">
                     <div class="card-header">
                         <h4>User</h4>

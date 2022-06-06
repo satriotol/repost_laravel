00c15f278214b1da510a0e410839577e116207c6
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-06-06 09:46:22 +0700

    fixing

--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -90,6 +90,7 @@
     </div>
 @endsection
 @push('script')
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
     {{-- <script>
         $(document).ready(function() {
             $('.myTable2').DataTable({
